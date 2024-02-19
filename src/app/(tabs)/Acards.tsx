29f334fe229510c9819d00b9cea3fa4a5d25cf22
dolgoonnn <dolgoon.da@gmail.com>
2024-02-19 17:22:07 +0800

    map explore page functionality is done and navigation integration

--- src/app/(tabs)/Acards.tsx
+++ src/app/(tabs)/Acards.tsx
@@ -1,23 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Color from '../constants/Color'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Color from "../constants/Color";
+import RestaurantMapView from "../components/sections/RestaurantMapView";
 
 const Acards = () => {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor:Color.base.White }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: Color.base.White }}>
       <View style={styles.container}>
-        <Text>Acards</Text>
+        <RestaurantMapView />
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Acards
+export default Acards;
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: Color.base.White,
-  }
-})
+  },
+});
