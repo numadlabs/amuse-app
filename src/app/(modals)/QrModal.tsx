99b6da52a64484939ca70ea76076d76ad37b082f
dolgoonnn <dolgoon.da@gmail.com>
2024-02-22 14:23:45 +0800

    qrmodal screen

--- src/app/(modals)/QrModal.tsx
+++ src/app/(modals)/QrModal.tsx
@@ -1,12 +1,226 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { CameraView, Camera } from "expo-camera/next";
+import Color from "../constants/Color";
+import { useRouter } from "expo-router";
+
+const { width, height } = Dimensions.get("window");
+
+const markerSize = 250;
+const halfMarkerSize = markerSize / 2;
 
 const QrModal = () => {
-  return (
-    <View>
-      <Text>QrModal</Text>
-    </View>
-  )
-}
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [flashMode, setFlashMode] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
 
-export default QrModal
+    getCameraPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  function marker(
+    color: string,
+    size: number,
+    borderLength: number,
+    thickness: number = 2,
+    borderRadius: number = 0,
+  ): JSX.Element {
+    return (
+      <View style={{ height: size, width: size }}>
+        <View
+          style={{
+            position: "absolute",
+            height: borderLength,
+            width: borderLength,
+            top: 0,
+            left: 0,
+            borderColor: color,
+            borderTopWidth: thickness,
+            borderLeftWidth: thickness,
+            borderTopLeftRadius: borderRadius,
+          }}
+        ></View>
+        <View
+          style={{
+            position: "absolute",
+            height: borderLength,
+            width: borderLength,
+            top: 0,
+            right: 0,
+            borderColor: color,
+            borderTopWidth: thickness,
+            borderRightWidth: thickness,
+            borderTopRightRadius: borderRadius,
+          }}
+        ></View>
+        <View
+          style={{
+            position: "absolute",
+            height: borderLength,
+            width: borderLength,
+            bottom: 0,
+            left: 0,
+            borderColor: color,
+            borderBottomWidth: thickness,
+            borderLeftWidth: thickness,
+            borderBottomLeftRadius: borderRadius,
+          }}
+        ></View>
+        <View
+          style={{
+            position: "absolute",
+            height: borderLength,
+            width: borderLength,
+            bottom: 0,
+            right: 0,
+            borderColor: color,
+            borderBottomWidth: thickness,
+            borderRightWidth: thickness,
+            borderBottomRightRadius: borderRadius,
+          }}
+        ></View>
+      </View>
+    );
+  }
+
+  const overlayStyles = (
+    overlayWidth,
+    overlayHeight,
+    marginTop = 0,
+    marginLeft = 0,
+  ) => ({
+    ...styles.overlay,
+    width: overlayWidth,
+    height: overlayHeight,
+    marginTop,
+    marginLeft,
+  });
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <CameraView
+          onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+          barcodeScannerSettings={{
+            barcodeTypes: ["qr", "pdf417"],
+          }}
+          style={StyleSheet.absoluteFillObject}
+          flash={flashMode == true ? "on" : "off"}
+          // flash="on"
+        />
+
+        <View style={overlayStyles(width, (height - markerSize) / 2)} />
+        <View
+          style={overlayStyles(
+            width,
+            (height - markerSize) / 2,
+            (height + markerSize) / 2,
+          )}
+        />
+        <View
+          style={overlayStyles(
+            (width - markerSize) / 2,
+            markerSize,
+            (height - markerSize) / 2,
+          )}
+        />
+        <View
+          style={overlayStyles(
+            (width - markerSize) / 2,
+            markerSize,
+            (height - markerSize) / 2,
+            (width + markerSize) / 2,
+          )}
+        />
+        <View style={styles.markerContainer}>
+          {marker("white", 257, 60, 4, 12)}
+
+          <TouchableOpacity
+            style={[styles.button, styles.flashButton]}
+            onPress={() => {
+              setFlashMode(!flashMode);
+            }}
+          >
+            <Image source={require("@/public/icons/flash.png")} />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={[styles.button, styles.closeButton]}
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <Image source={require("@/public/icons/close.png")} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  overlay: {
+    position: "absolute",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    // borderRadius: 20,
+  },
+  markerContainer: {
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    marginLeft: -halfMarkerSize,
+    marginTop: -halfMarkerSize,
+    height: markerSize,
+    width: markerSize,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 48,
+    padding: 12,
+    borderRadius: 100,
+    backgroundColor: Color.Gray.gray50,
+  },
+  closeButton: {
+    position: "absolute",
+    right: "0%",
+    top: "0%",
+    marginTop: 4,
+    marginRight: 16,
+  },
+  flashButton: {
+    marginTop: 100,
+  },
+});
+
+export default QrModal;
