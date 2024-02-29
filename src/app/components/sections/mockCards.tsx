f99a1ec68b10b00b3424ac27d197e89d72aa7700
ganaa0910 <ganerdene.dev@gmail.com>
2024-02-29 13:47:48 +0800

    profile sign out

---
+++ src/app/components/sections/mockCards.tsx
@@ -0,0 +1,22 @@
+const mockCards = [
+  {
+    id: 1,
+    name: 'sushitrash',
+    image: '../../../public/images/Image1.png',
+    added: false,
+  },
+  {
+    id: 2,
+    name: 'gekkai genkai',
+    image: '../../../public/images/Image1.png',
+    added: true,
+  },
+  {
+    id: 3,
+    name: 'nuas bros remedies',
+    image: '../../../public/images/Image1.png',
+    added: true,
+  },
+];
+
+export default mockCards;
