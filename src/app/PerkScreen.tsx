fc2451793761930f317496b1c282aee53cfd8d41
Dalai1002 <dalai.dev1002@gmail.com>
2024-05-05 19:26:53 +0800

    Perk Update

--- src/app/PerkScreen.tsx
+++ src/app/PerkScreen.tsx
@@ -1,22 +1,28 @@
-import { View, Text, ActivityIndicator, StyleSheet, Pressable } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { router, useLocalSearchParams } from 'expo-router'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { getRestaurantById } from './lib/service/queryHelper'
-import APassCard from './components/atom/cards/APassCard'
-import Button from './components/ui/Button'
-import { restaurantKeys, userKeys } from './lib/service/keysHelper'
-import Animated, { SlideInDown, SlideOutDown } from 'react-native-reanimated'
-import { height, width } from './lib/utils'
-import PowerUpCard from './components/atom/cards/PowerUpCard'
-import { LinearGradient } from 'expo-linear-gradient'
-import Color from './constants/Color'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  Pressable,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getRestaurantById } from "./lib/service/queryHelper";
+import APassCard from "./components/atom/cards/APassCard";
+import Button from "./components/ui/Button";
+import { restaurantKeys, userKeys } from "./lib/service/keysHelper";
+import Animated, { SlideInDown, SlideOutDown } from "react-native-reanimated";
+import { height, width } from "./lib/utils";
+import PowerUpCard from "./components/atom/cards/PowerUpCard";
+import { LinearGradient } from "expo-linear-gradient";
+import Color from "./constants/Color";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const PerkScreen = () => {
-  const { restaurantId, btcAmount, powerUp } = useLocalSearchParams()
+  const { restaurantId, btcAmount, powerUp } = useLocalSearchParams();
 
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
   const { data: card = [], isLoading } = useQuery({
     queryKey: [restaurantKeys.detail],
@@ -26,25 +32,33 @@ const PerkScreen = () => {
   });
 
   const handleNavigation = async () => {
-    router.back()
+    router.back();
     queryClient.invalidateQueries({ queryKey: userKeys.info });
     try {
-      await AsyncStorage.setItem('restaurantCard', JSON.stringify(card));
-      console.log('Card information stored successfully.');
+      await AsyncStorage.setItem("restaurantCard", JSON.stringify(card));
+      console.log("Card information stored successfully.");
     } catch (error) {
-      console.log('Error storing card information:', error);
+      console.log("Error storing card information:", error);
     }
-  }
+  };
 
   return (
-    <Animated.View entering={SlideInDown.springify().damping(20)} style={styles.container}>
+    <Animated.View
+      entering={SlideInDown.springify().damping(20)}
+      style={styles.container}
+    >
       {isLoading && (
-        <View style={{ flex:1,justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
           <ActivityIndicator />
         </View>
-
-      )
-      }
+      )}
 
       {!isLoading && card && (
         <>
@@ -58,57 +72,90 @@ const PerkScreen = () => {
             visitCount={card.visitCount}
           />
 
-          <Animated.View style={{ gap: 16, marginBottom: 10 }}
-            entering={SlideInDown.springify().damping(20).delay(200)}>
+          <Animated.View
+            style={{ marginBottom: 10, flexDirection: 'column' }}
+            entering={SlideInDown.springify().damping(20).delay(200)}
+          >
             <LinearGradient
               colors={[Color.Brand.card.start, Color.Brand.card.end]}
               start={{ x: 1, y: 0 }}
               end={{ x: 1, y: 1 }}
-              style={{ borderRadius: 16, justifyContent: 'center', height: height / 6, gap: 12, alignContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: Color.Gray.gray400, }}>
-              <Text style={{ fontWeight: '700', fontSize: 24, lineHeight: 32, color: Color.base.White }}>
+              style={{
+                borderRadius: 16,
+                justifyContent: "center",
+                height: height / 6,
+                gap: 12,
+                alignContent: "center",
+                alignItems: "center",
+                borderWidth: 1,
+                borderColor: Color.Gray.gray400,
+                marginBottom: 16
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: "700",
+                  fontSize: 24,
+                  lineHeight: 32,
+                  color: Color.base.White,
+                }}
+              >
                 +$1 of BTC
               </Text>
-              <Text style={{ fontWeight: '400', fontSize: 14, lineHeight: 18, color: Color.Gray.gray50 }}>
+              <Text
+                style={{
+                  fontWeight: "400",
+                  fontSize: 14,
+                  lineHeight: 18,
+                  color: Color.Gray.gray50,
+                }}
+              >
                 Check-in successful.
               </Text>
             </LinearGradient>
-            {powerUp ?
-             <View style={{height:height/8}}>
-
-          
-              <PowerUpCard title={powerUp as string} onPress={() => router.navigate('/PowerUp')} />
+            {powerUp ? (
+              <View style={{ height: height / 8 }}>
+                <PowerUpCard
+                  title={powerUp as string}
+                  onPress={() => router.navigate("/PowerUp")}
+                />
               </View>
-            : <View style={{height:height/8}}/>}
+            ) : ( ""
+            )}
           </Animated.View>
-          <Button variant='primary' size='default' textStyle='primary' onPress={handleNavigation} style={{ bottom: 30 }}>
-            <Text>
-              Confirm
-            </Text>
+          <Button
+            variant="primary"
+            size="default"
+            textStyle="primary"
+            onPress={handleNavigation}
+            style={{ bottom: 30 }}
+          >
+            <Text>Confirm</Text>
           </Button>
         </>
       )}
     </Animated.View>
-  )
-}
+  );
+};
 
-export default PerkScreen
+export default PerkScreen;
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: height / 10,
+    marginTop: height / 6.5,
     backgroundColor: Color.Gray.gray600,
     paddingHorizontal: 16,
     flex: 1,
-    gap: 16
+    gap: 16,
   },
   cardContainer: {
-    alignItems: 'center',
-    alignContent: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "center",
   },
   buttonContainer: {
-    position: 'absolute',
+    position: "absolute",
     width: "100%",
     bottom: 30,
   },
-})
+});
