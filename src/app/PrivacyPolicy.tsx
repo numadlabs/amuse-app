62b84b685c6dd7238fdff0a2d42fcb90e95d1e20
ganaa0910 <ganerdene.dev@gmail.com>
2024-08-27 17:10:51 +0800

    privacy and policy

--- src/app/PrivacyPolicy.tsx
+++ src/app/PrivacyPolicy.tsx
@@ -230,41 +230,48 @@ const TermsAndCondo = () => {
     {
       title: "Data Collection",
       text: `The Platform collects two types of data from its users: necessary data collection which cannot be disabled and optional data collection which may be enabled/disabled at your discretion.  
-          I. Location: We use location data to assist you in locating restaurants participating in a program on the Platform. We may store location data to improve and optimize the Platform. 
-          II. Email: We will store and use your email for account creation, user login, password management, and Platform-to-user communications. 
-C. Optional Data Collection 
-          I. Birthday: We use birthday data to allow us to offer you a special birthday promotion or reward. 
-          II. Profile Picture: You may opt to add a profile picture in order to personalize your profile. If added, the Platform will automatically store the data.`,
+
+Location: We use location data to assist you in locating restaurants participating in a program on the Platform. We may store location data to improve and optimize the Platform. 
+
+Email: We will store and use your email for account creation, user login, password management, and Platform-to-user communications. 
+
+Optional Data Collection: 
+
+Birthday: We use birthday data to allow us to offer you a special birthday promotion or reward. 
+
+Profile Picture: You may opt to add a profile picture in order to personalize your profile. If added, the Platform will automatically store the data.`,
     },
     {
       title: "General Data Protection Regulation",
-      text: `A. Our legal basis for collecting and using the data is for one or more of the following purposes: 
-          I. We need to perform a contract with you. 
-          II. You have given us permission to do so. 
-          III. The processing is in our legitimate interest, and it is not overridden by your rights. 
-          IV. It is necessary for payment processing purposes. 
-          V. It is necessary to comply with the law.`,
+      text: `Our legal basis for collecting and using the data is for one or more of the following purposes:
+	•	We need to perform a contract with you.
+	•	You have given us permission to do so. 
+	•	The processing is in our legitimate interest, and it is not overridden by your rights. 
+	•	It is necessary for payment processing purposes. 
+	•	It is necessary to comply with the law. `,
     },
     {
       title: "Data Retention and Disclosure",
-      text: `A. We will retain data only for as long as is necessary for the purposes as stated in this policy. 
-B. Disclosure of your data may occur for one or more of the following reasons: 
-          I. The Platform is involved in a merger, acquisition, or asset sale.  
-          II. We are required by law to disclose your data.  
-          III. We have a good faith belief that it is necessary to disclose your data in relation to the protection of the Platform and/or legal matters, both potential and active.`,
+      text: `	We will retain data only for as long as is necessary for the purposes as stated in this policy. 
+	•	Disclosure of your data may occur for one or more of the following reasons:
+	•	The Platform is involved in a merger, acquisition or asset sale. 
+	•	We are required by law to disclose your data. 
+	•	We have a good faith belief that it is necessary to disclose your data in relation to the protection of the Platform and/or legal matters, both potential and active. `,
     },
     {
       title: "Data Protection",
-      text: `A. The security of your data is important to us, and we strive to use all commercially reasonable means to protect your data. However, we do not guarantee absolute security of your data.`,
+      text: `The security of your data is important to us, and we strive to use all commercially reasonable means to protect your data. However, we do not guarantee absolute security of your data.`,
     },
     {
       title: "User Rights",
-      text: `A. You have the following rights regarding your data: 
-          I. Access: You can request access to the data of yours that we have collected.  
-          II. Rectification: You have the right to have your information corrected if it is inaccurate or incomplete.  
-          III. Objection: You have the right to request that we restrict the processing of your data.  
-          IV. Portability: You have the right to be provided with a copy of your data that we have collected.  
-          V. Withdraw Consent: You have the right to withdraw your consent at any time to the collection of your data.`,
+      text: `You have the following rights regarding your data: 
+	•	Access: You can request access to the data of yours that we have collected. 
+	•	Rectification: You have the right to have your information corrected
+that information is inaccurate or incomplete. 
+	•	Objection: You have the right to request that we restrict the processing of your data. 
+	•	Portability: You have the right to be provided with a copy of your data that we have collected. 
+	•	Withdraw Consent: You have the right to withdraw your consent at any time to the collection of your data. 
+`,
     },
   ];
 
