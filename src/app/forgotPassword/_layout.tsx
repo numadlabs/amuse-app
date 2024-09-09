83df142ddf5567c60ecffbe70bfd1138349141b6
ganaa0910 <ganerdene.dev@gmail.com>
2024-09-09 22:22:50 +0800

    responsive update

--- src/app/forgotPassword/_layout.tsx
+++ src/app/forgotPassword/_layout.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Color from "@/constants/Color";
 
 export default function FpLayout() {
   return (
     <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Email" />
-        <Stack.Screen name="VerificationCode" />
-        <Stack.Screen name="NewPassword" />
-        <Stack.Screen name="Success" options={{ headerShown: false }} />
-      </Stack>
+      <SafeAreaView style={{ flex: 1, backgroundColor: Color.Gray.gray600 }}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Email" />
+          <Stack.Screen name="VerificationCode" />
+          <Stack.Screen name="NewPassword" />
+          <Stack.Screen name="Success" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaView>
     </>
   );
 }
--- src/app/signUp/_layout.tsx
+++ src/app/signUp/_layout.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import { Stack } from "expo-router";
 import { Platform, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Color from "@/constants/Color";
 
 export default function SuLayout() {
   return (
     <>
-     
+      <SafeAreaView style={{flex: 1, backgroundColor: Color.Gray.gray600}}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Email" />
         <Stack.Screen name="Otp" options={{ presentation: "modal" }} />
         <Stack.Screen name="Password" />
         <Stack.Screen name="NickName" />
       </Stack>
-
+      </SafeAreaView>
     </>
   );
 }
