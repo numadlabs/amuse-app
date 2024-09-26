2f8aea95dd0a244be714f2e329c477e867ab1967
ganaa0910 <ganerdene.dev@gmail.com>
2024-09-26 17:31:04 +0800

    no internet check logic

--- src/app/NoInternet.tsx
+++ src/app/NoInternet.tsx
@@ -3,29 +3,30 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { EmojiSad } from 'iconsax-react-native'
 import Color from '@/constants/Color'
-import { BODY_2_REGULAR, H6 } from '@/constants/typography'
+import { BODY_2_REGULAR, BUTTON_48, H6 } from '@/constants/typography'
 import Button from '@/components/ui/Button'
 
 const NoInternet = ({ onPress }) => {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor:Color.Gray.gray600 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: Color.Gray.gray600 }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <View style={{ padding: 16, backgroundColor: Color.Gray.gray500, borderRadius: 16 }}>
           <EmojiSad size={48} color={Color.Gray.gray100} />
         </View>
-
-        <Text style={{ ...H6, color: Color.base.White, textAlign: 'center', marginTop: 16 }}>
-          No internet{"\n"}
-          connection available
-        </Text>
-        <Text style={{ ...BODY_2_REGULAR, color: Color.Gray.gray100, textAlign: 'center', marginTop: 8 }}>
-          Please check your internet connection and try again
-        </Text>
+        <View style={{gap:12}}>
+          <Text style={{ ...H6, color: Color.base.White, textAlign: 'center', marginTop: 16 }}>
+            No internet{"\n"}
+            connection available
+          </Text>
+          <Text style={{ ...BODY_2_REGULAR, color: Color.Gray.gray100, textAlign: 'center', marginTop: 8 }}>
+            Please check your internet connection and try again
+          </Text>
+        </View>
       </View>
 
       <View style={{ padding: 16 }}>
         <Button variant='disabled' onPress={onPress}>
-          <Text>
+          <Text style={{ ...BUTTON_48, color: Color.base.White }}>
             Try again
           </Text>
         </Button>
