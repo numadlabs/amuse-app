e38209988799be7a14726d488ebf147f5369daa8
ganaa0910 <ganerdene.dev@gmail.com>
2024-09-30 18:13:03 +0800

    production

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -170,7 +170,7 @@ function Login() {
                     marginBottom: 24,
                   }}
                 >
-                  Welcomeeeeeee
+                  Welcome
                 </Text>
 
                 <View style={{ gap: 12 }}>
