4c27e86001ca1fc523b5cd5f1c56563a766af029
dolgoonnn <dolgoon.da@gmail.com>
2024-09-30 12:45:15 +0800

    layout hook error fixed and warning removed

---
+++ src/lib/auth-utils.tsx
@@ -0,0 +1,30 @@
+import { SERVER_SETTING } from "@/constants/serverSettings";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { QueryCache } from "@tanstack/react-query";
+import { router } from "expo-router";
+import { deleteUserId } from "./service/asyncStorageHelper";
+import * as SecureStore from "expo-secure-store";
+
+const queryCache = new QueryCache();
+
+/**
+ * Logs out the user by deleting the access and refresh tokens from secure storage and navigating to the login screen.
+ *
+ * @remarks
+ * This function is called when the user's access token has expired and a refresh token is available.
+ * It deletes the tokens from secure storage, clears the axios client's authorization header, and redirects the user to the login screen.
+ *
+ * @returns {Promise<void>} - A promise that resolves when the logout process is complete.
+ */
+export async function logoutHandler(axiosClient) {
+  // Update HTTP Headers
+  axiosClient.defaults.headers.common["Authorization"] = "";
+  await SecureStore.deleteItemAsync(SERVER_SETTING.TOKEN_KEY);
+  await SecureStore.deleteItemAsync(SERVER_SETTING.REFRESH_TOKEN_KEY);
+  //TODO key object dotroos duudah
+  await AsyncStorage.removeItem("hasSeenWelcomeMessage");
+  await deleteUserId();
+  queryCache.clear();
+
+  router.replace("/Login");
+}
