4afba0dabb9c24ccbc16684a939753abd49317f4
ganaa0910 <ganerdene.dev@gmail.com>
2024-09-30 18:22:45 +0800

    production3

--- src/app/TermsAndCondo.tsx
+++ src/app/TermsAndCondo.tsx
@@ -22,61 +22,68 @@ const TermsAndCondo = () => {
   ];
 
   return (
-    <SafeAreaView style={{flex: 1,  backgroundColor: Color.Gray.gray600}}>
-    <View style={{ backgroundColor: Color.Gray.gray600, flex: 1 }}>
-      <Header title="Terms and Conditions" />
-      <ScrollView style={styles.container}>
-        {/* {data.map((item, index) => (
+    <SafeAreaView style={{ flex: 1, backgroundColor: Color.Gray.gray600 }}>
+      <View style={{ backgroundColor: Color.Gray.gray600, flex: 1 }}>
+        <Header title="Terms and Conditions" />
+        <ScrollView style={styles.container}>
+          {/* {data.map((item, index) => (
           <Accordion key={index} title={item.title} text={item.text} />
         ))} */}
           <Text style={styles.paragraph}>
-                      Welcome to Amuse Bouche! We are excited to welcome you to our growing community! {"\n"}
-                    </Text>
-                    <Text style={styles.paragraph}>
-                      We’re thrilled to have you join our Pilot Program, and we greatly
-                      appreciate your participation. This program allows us to refine Amuse
-                      Bouche’s features, ensuring it becomes the best experience possible
-                      for our entire community, including you!
-                    </Text>
+            Welcome to Amuse Bouche! {"\n"}
+          </Text>
+          <Text style={styles.paragraph}>
+            We’re thrilled to have you join our Pilot Program, and we greatly
+            appreciate your participation. This program allows us to refine Amuse
+            Bouche’s features, ensuring it becomes the best experience possible
+            for our entire community, including you!
+          </Text>
 
-                    <Text style={styles.paragraph}>
-                      Here at Amuse Bouche, we value transparency with our users. So,
-                      please note that while using the Amuse Bouche Application, certain
-                      user data will be collected. To enable account creation and continued
-                      user access, it is necessary that user email data is collected.
-                      Additionally, user experience is unique to each location, which
-                      requires user location data to also be collected.
-                    </Text>
+          <Text style={styles.paragraph}>
+            Here at Amuse Bouche, we value transparency with our users. So,
+            please note that while using the Amuse Bouche Application, certain
+            user data will be collected. To enable account creation and continued
+            user access, it is necessary that user email data is collected.
+            Additionally, user experience is unique to each location, which
+            requires user location data to also be collected.
+          </Text>
 
-                    <Text style={styles.paragraph}>
-                      Aside from user email and location data collection, the rest is up to
-                      you! You can opt to allow the collection of data such as your birthday
-                      and profile picture. Opting-in allows us here at Amuse Bouche to
-                      continue to improve the Application so we can provide a more
-                      seamless and tailored user experience for you.
-                    </Text>
+          <Text style={styles.paragraph}>
+            Aside from user email and location data collection, the rest is up to
+            you! You can opt to allow the collection of data such as your birthday
+            and profile picture. Opting-in allows us here at Amuse Bouche to
+            continue to improve the Application so we can provide a more
+            seamless and tailored user experience for you.
+          </Text>
 
-                    <Text style={styles.paragraph}>
-                      Your privacy is important, and what data you choose to disclose is
-                      totally up to you! To change your data collection preferences, you can
-                      go to the privacy section of the settings menu and view the data
-                      collection options.
-                    </Text>
+          <Text style={styles.paragraph}>
+            Your privacy is important, and what data you choose to disclose is
+            totally up to you! To change your data collection preferences, you can
+            go to the privacy section of the settings menu and view the data
+            collection options.
+          </Text>
+          <Text style={styles.paragraph}>
+            Disclaimer: The Amuse Bouche Application is solely a platform for
+            third-parties to engage with users. Any offerings of rewards or
+            securities accessible through the Application are provided by third-
+            parties. Hash2 Labs LLC, the developer of the Application, is not liable
+            for any offers, rewards or any associated claims, damages or losses.
+          </Text>
 
-                    <Text style={styles.paragraph}>
-                      One final note, to ensure security and smooth operations during the
-                      Pilot Program, some features will be limited. Specifically, you won’t be
-                      able to withdraw or transfer any bitcoin earned until the Pilot Program
-                      ends. We’ll notify all users via email and app notification as soon as
-                      the Pilot Program is completed.
-                    </Text>
+          <Text style={styles.paragraph}>
+            One final note, to ensure security and smooth operations during the
+            Pilot Program, some features will be limited. Specifically, you won’t be
+            able to withdraw or transfer any bitcoin earned until the Pilot Program
+            ends. We’ll notify all users via email and app notification as soon as
+            the Pilot Program is completed.
+          </Text>
 
-                    <Text style={[styles.paragraph, { marginTop: 44 }]}>
-                      We are excited to have you as a part of our growing community!
-                    </Text>
+          <Text style={[styles.paragraph, { marginTop: 44 }]}>
+            We are excited to have you as a part of our growing community!
+          </Text>
 
-      </ScrollView>
-    </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
